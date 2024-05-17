c11ebe9b8e4f66f4db791ca354cdddc291e2601a
Andri Febriyadi Putra <andrifebriyadiputra@gmail.com>
2024-05-17 20:12:47 +0700

    feat: fixed the reservation logic, the facility will not be available during the reservation time

--- src/main/java/com/enigmacamp/reservationcampus/services/impl/TransactionServiceImpl.java
+++ src/main/java/com/enigmacamp/reservationcampus/services/impl/TransactionServiceImpl.java
@@ -1,23 +1,15 @@
 package com.enigmacamp.reservationcampus.services.impl;
 
-import com.enigmacamp.reservationcampus.model.entity.Facility;
-import com.enigmacamp.reservationcampus.model.entity.Profile;
-import com.enigmacamp.reservationcampus.model.entity.Transaction;
-import com.enigmacamp.reservationcampus.model.entity.TransactionDetail;
-import com.enigmacamp.reservationcampus.model.entity.constant.Availability;
-import com.enigmacamp.reservationcampus.model.entity.constant.Penalties;
-import com.enigmacamp.reservationcampus.model.entity.constant.StatusReservation;
+import com.enigmacamp.reservationcampus.model.entity.*;
+import com.enigmacamp.reservationcampus.model.entity.constant.*;
 import com.enigmacamp.reservationcampus.model.request.TransactionRequest;
 import com.enigmacamp.reservationcampus.model.response.TransactionDTO;
 import com.enigmacamp.reservationcampus.model.response.TransactionDetailDTO;
-import com.enigmacamp.reservationcampus.repository.PenaltiesRepository;
+import com.enigmacamp.reservationcampus.repository.*;
 import com.enigmacamp.reservationcampus.repository.constant.AvailabilityRepository;
 import com.enigmacamp.reservationcampus.repository.constant.StatusRepository;
-import com.enigmacamp.reservationcampus.repository.TransactionRepository;
 import com.enigmacamp.reservationcampus.services.*;
-import com.enigmacamp.reservationcampus.utils.constant.EAvailability;
-import com.enigmacamp.reservationcampus.utils.constant.EPenalties;
-import com.enigmacamp.reservationcampus.utils.constant.EStatusReservation;
+import com.enigmacamp.reservationcampus.utils.constant.*;
 import com.enigmacamp.reservationcampus.utils.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -42,202 +34,153 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     @Transactional
-    public Transaction saveTransaction(TransactionRequest transaction) {
-        String profileId = transaction.getId_profile();
+    public Transaction saveTransaction(TransactionRequest transactionRequest) {
+        String profileId = transactionRequest.getId_profile();
         Profile profile = profileService.getProfileById(profileId);
         StatusReservation status = statusRepository.findByStatus(EStatusReservation.STATUS_PROCESSED);
         Penalties penalties = penaltiesRepository.findByName(EPenalties.NOT_PENALTY);
 
-        Availability notAvailability = availabilityRepository.findByName(EAvailability.AVAILABILITY_NO);
-
-        Transaction transaction1 = new Transaction();
-        transaction1.setSubject(transaction.getSubject());
-        transaction1.setDocument(transaction.getDocument());
-        transaction1.setProfile(profile);
-        transaction1.setDateReservation(transaction.getDateReservation());
-        transaction1.setDateSubmission(Date.valueOf(LocalDate.now()));
-        transaction1.setDateReturn(transaction.getDateReturn());
-        transaction1.setStatus(status);
-        transaction1.setPenalties(penalties);
-
-        Transaction transactionResult = transactionRepository.save(transaction1);
-        List<TransactionDetail> transactionDetailList = new ArrayList<>();
-
-        for(TransactionDetailDTO transactionDetail : transaction.getTransactionDetail()) {
-            TransactionDetail transactionDetail1 = new TransactionDetail();
-            transactionDetail1.setTransaction(transaction1);
-            transactionDetail1.setPrice(transactionDetail.getPrice());
-            transactionDetail1.setQuantity(transactionDetail.getQuantity());
-            transactionDetail1.setFacility(facilityService.getFacilityById(transactionDetail.getId()));
-
-            Facility facility = facilityService.getFacilityById(transactionDetail.getId());
-            Integer stok = facility.getQuantity();
-            Integer quantity = transactionDetail.getQuantity();
-            if(stok == 0 || stok < quantity){
-                facility.setAvailability(notAvailability);
-                throw new RuntimeException("STOK TIDAK CUKUP");
-            }else{
-                facility.setQuantity(stok - quantity);
-                facilityService.updateFacility(facility);
+        // Check availability of each facility in the request
+        for (TransactionDetailDTO detailDTO : transactionRequest.getTransactionDetail()) {
+            if (!isFacilityAvailable(detailDTO.getId(), transactionRequest.getDateReservation(), transactionRequest.getDateReturn())) {
+                throw new RuntimeException("Facility is not available for the selected dates");
             }
-            transactionDetailService.saveTransactionDetail(transactionDetail1);
-            transactionDetailList.add(this.transactionDetailService.saveTransactionDetail(transactionDetail1));
         }
-        transactionResult.setTransactionDetail(transactionDetailList);
-        return transactionResult;
 
+        Transaction transaction = new Transaction();
+        transaction.setSubject(transactionRequest.getSubject());
+        transaction.setDocument(transactionRequest.getDocument());
+        transaction.setProfile(profile);
+        transaction.setDateSubmission(Date.valueOf(LocalDate.now()));
+        transaction.setDateReservation(transactionRequest.getDateReservation());
+        transaction.setDateReturn(transactionRequest.getDateReturn());
+        transaction.setStatus(status);
+        transaction.setPenalties(penalties);
+
+        Transaction savedTransaction = transactionRepository.save(transaction);
+
+        List<TransactionDetail> transactionDetailList = new ArrayList<>();
+        for (TransactionDetailDTO detailDTO : transactionRequest.getTransactionDetail()) {
+            TransactionDetail detail = new TransactionDetail();
+            detail.setTransaction(savedTransaction);
+            detail.setPrice(detailDTO.getPrice());
+            detail.setQuantity(detailDTO.getQuantity());
+            Facility facility = facilityService.getFacilityById(detailDTO.getId());
+            detail.setFacility(facility);
+
+            // Reduce facility stock
+            facility.setQuantity(facility.getQuantity() - detailDTO.getQuantity());
+            facilityService.updateFacility(facility);
+
+            transactionDetailList.add(transactionDetailService.saveTransactionDetail(detail));
+        }
+
+        savedTransaction.setTransactionDetail(transactionDetailList);
+        return savedTransaction;
+    }
+
+    private boolean isFacilityAvailable(String facilityId, Date startDate, Date endDate) {
+        List<Transaction> reservations = transactionRepository.findReservationsForFacilityWithinDates(facilityId, startDate, endDate);
+        return reservations.isEmpty();
     }
 
     @Override
     public TransactionDTO getTransactionById(String id) {
-        if(transactionRepository.findById(id).isPresent()){
-            Transaction transaction = transactionRepository.findById(id).get();
-            TransactionDTO transactionDTO = new TransactionDTO();
-            Profile profile = profileService.getProfileById(transaction.getProfile().getId());
-
-            transactionDTO.setId(transaction.getId());
-            transactionDTO.setName(profile.getFullName());
-            transactionDTO.setDocument(transaction.getDocument());
-            transactionDTO.setSubject(transaction.getSubject());
-            transactionDTO.setDateReservation(transaction.getDateReservation());
-            transactionDTO.setDateSubmission(transaction.getDateSubmission());
-            transactionDTO.setDateReturn(transaction.getDateReturn());
-
-            List<TransactionDetailDTO> transactionDetailDtoList = new ArrayList<>();
-
-            int totalItem = 0;
-            int grandTotal = 0;
-
-            for(TransactionDetail transactionDetail : transaction.getTransactionDetail()){
-                TransactionDetailDTO transactionDetailDTO = new TransactionDetailDTO();
-                transactionDetailDTO.setId(transactionDetail.getId());
-                transactionDetailDTO.setPrice(transactionDetail.getPrice());
-                transactionDetailDTO.setQuantity(transactionDetail.getQuantity());
-                Facility facility = facilityService.getFacilityById(transactionDetail.getFacility().getId());
-                transactionDetailDTO.setName(facility.getName());
-
-                totalItem += transactionDetail.getQuantity();
-                grandTotal += transactionDetail.getQuantity() * transactionDetail.getPrice();
-
-                transactionDetailDtoList.add(transactionDetailDTO);
-
-            }
-            transactionDTO.setTransactionDetailDTO(transactionDetailDtoList);
-            transactionDTO.setTotalItem(totalItem);
-            transactionDTO.setGrandTotal(grandTotal);
-
-            return transactionDTO;
-        }else{
-            throw new DataNotFoundException("Data not Found");
-        }
-
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Data not found"));
+        return convertToDTO(transaction);
     }
 
     @Override
     public List<TransactionDTO> getAllTransaction() {
         List<Transaction> transactions = transactionRepository.findAll();
         List<TransactionDTO> transactionDTOList = new ArrayList<>();
-
         for (Transaction transaction : transactions) {
-            Profile profile = profileService.getProfileById(transaction.getProfile().getId());
-
-            List<TransactionDetailDTO> transactionDetailDTOList = new ArrayList<>();
-            for (TransactionDetail detail : transaction.getTransactionDetail()) {
-                transactionDetailDTOList.add(TransactionDetailDTO.fromEntity(detail));
-            }
-
-            TransactionDTO transactionDTO = TransactionDTO.builder()
-                    .id(transaction.getId())
-                    .name(profile.getFullName())
-                    .document(transaction.getDocument())
-                    .subject(transaction.getSubject())
-                    .dateSubmission(transaction.getDateSubmission())
-                    .dateReservation(transaction.getDateReservation())
-                    .dateReturn(transaction.getDateReturn())
-                    .status(transaction.getStatus().getStatus().name())
-                    .penalties(transaction.getPenalties().getName().name())
-                    .transactionDetailDTO(transactionDetailDTOList)
-                    .build();
-
-            transactionDTOList.add(transactionDTO);
+            transactionDTOList.add(convertToDTO(transaction));
         }
-
         return transactionDTOList;
     }
 
-
-
-
-
     @Override
-    public List<TransactionDTO> findTransactionsbyName(String name) {
-        List<Transaction> transactions = transactionRepository.findBySubject(name);
+    public List<TransactionDTO> findTransactionsbyName(String subject) {
+        List<Transaction> transactions = transactionRepository.findBySubject(subject);
         List<TransactionDTO> transactionDTOList = new ArrayList<>();
-
-        TransactionDTO transactionDTO = new TransactionDTO();
-
-        for(Transaction transaction : transactions){
-            Profile profile = profileService.getProfileById(transaction.getProfile().getId());
-            transactionDTO.setId(transaction.getId());
-            transactionDTO.setName(profile.getFullName());
-            transactionDTO.setDocument(transaction.getDocument());
-            transactionDTO.setSubject(transaction.getSubject());
-            transactionDTO.setDateReservation(transaction.getDateReservation());
-            transactionDTO.setDateSubmission(transaction.getDateSubmission());
-            transactionDTO.setDateReturn(transaction.getDateReturn());
-
-            transactionDTOList.add(transactionDTO);
+        for (Transaction transaction : transactions) {
+            transactionDTOList.add(convertToDTO(transaction));
         }
         return transactionDTOList;
-
     }
 
     @Override
     @Transactional
     public Transaction updateTransaction(Transaction transaction) {
-        Transaction transaction1 = transactionRepository.findById(transaction.getId()).get();
-        transaction1.setSubject(transaction.getSubject());
-        transaction1.setDocument(transaction.getDocument());
-        transaction1.setProfile(transaction.getProfile());
-        transaction1.setDateReservation(transaction.getDateReservation());
-        transaction1.setDateSubmission(transaction.getDateSubmission());
-        transaction1.setDateReturn(transaction.getDateReturn());
-        transaction1.setStatus(transaction.getStatus());
-        transaction1.setPenalties(transaction.getPenalties());
+        Transaction existingTransaction = transactionRepository.findById(transaction.getId())
+                .orElseThrow(() -> new DataNotFoundException("Data not found"));
+        existingTransaction.setSubject(transaction.getSubject());
+        existingTransaction.setDocument(transaction.getDocument());
+        existingTransaction.setProfile(transaction.getProfile());
+        existingTransaction.setDateReservation(transaction.getDateReservation());
+        existingTransaction.setDateSubmission(transaction.getDateSubmission());
+        existingTransaction.setDateReturn(transaction.getDateReturn());
+        existingTransaction.setStatus(transaction.getStatus());
+        existingTransaction.setPenalties(transaction.getPenalties());
 
-        StatusReservation status = statusRepository.findById(transaction.getStatus().getId()).get();
-        if(status.getStatus() == EStatusReservation.STATUS_REJECTED || status.getStatus() == EStatusReservation.STATUS_CANCELED || status.getStatus() == EStatusReservation.STATUS_COMPLETED){
+        StatusReservation status = statusRepository.findById(transaction.getStatus().getId())
+                .orElseThrow(() -> new DataNotFoundException("Status not found"));
+        if (status.getStatus() == EStatusReservation.STATUS_REJECTED ||
+                status.getStatus() == EStatusReservation.STATUS_CANCELED ||
+                status.getStatus() == EStatusReservation.STATUS_COMPLETED) {
+
             for (TransactionDetail transactionDetail : transaction.getTransactionDetail()) {
                 Facility facility = facilityService.getFacilityById(transactionDetail.getFacility().getId());
                 facility.setQuantity(facility.getQuantity() + transactionDetail.getQuantity());
                 facilityService.updateFacility(facility);
             }
         }
-        return transactionRepository.save(transaction1);
+
+        return transactionRepository.save(existingTransaction);
     }
 
     @Override
     public void deleteTransaction(String id) {
-        Transaction transaction = transactionRepository.findById(id).get();
-        if(transaction.getId() != null){
-            transactionRepository.delete(transaction);
-        } else {
-            throw new DataNotFoundException("Data Not Found");
-        }
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Data not found"));
+        transactionRepository.delete(transaction);
     }
 
     @Override
     public void cancelTransaction(String id) {
-        Transaction transaction = transactionRepository.findById(id).get();
-        System.out.println(transaction);
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Data not found"));
         StatusReservation status = statusRepository.findByStatus(EStatusReservation.STATUS_CANCELED);
-        if(transaction.getId()!= null){
-            transaction.setStatus(status);
-            transactionRepository.save(transaction);
-        } else{
-            throw new DataNotFoundException("Data Not Found");
-        }
+        transaction.setStatus(status);
+        transactionRepository.save(transaction);
     }
 
+    private TransactionDTO convertToDTO(Transaction transaction) {
+        Profile profile = profileService.getProfileById(transaction.getProfile().getId());
 
+        List<TransactionDetailDTO> transactionDetailDTOList = new ArrayList<>();
+        for (TransactionDetail detail : transaction.getTransactionDetail()) {
+            transactionDetailDTOList.add(TransactionDetailDTO.fromEntity(detail));
+        }
+
+        int totalItem = transactionDetailDTOList.stream().mapToInt(TransactionDetailDTO::getQuantity).sum();
+        int grandTotal = transactionDetailDTOList.stream().mapToInt(dto -> dto.getPrice() * dto.getQuantity()).sum();
+
+        return TransactionDTO.builder()
+                .id(transaction.getId())
+                .name(profile.getFullName())
+                .subject(transaction.getSubject())
+                .document(transaction.getDocument())
+                .dateSubmission(transaction.getDateSubmission())
+                .dateReservation(transaction.getDateReservation())
+                .dateReturn(transaction.getDateReturn())
+                .status(transaction.getStatus().getStatus().name())
+                .penalties(transaction.getPenalties().getName().name())
+                .totalItem(totalItem)
+                .grandTotal(grandTotal)
+                .transactionDetailDTO(transactionDetailDTOList)
+                .build();
+    }
 }
