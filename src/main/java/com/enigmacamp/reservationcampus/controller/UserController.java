81b32e2db66986b8b1923cff85073175f99570a0
Andri Febriyadi Putra <andrifebriyadiputra@gmail.com>
2024-05-19 22:28:53 +0700

    feat: fix endpoint

--- src/main/java/com/enigmacamp/reservationcampus/controller/UserController.java
+++ src/main/java/com/enigmacamp/reservationcampus/controller/UserController.java
@@ -2,11 +2,16 @@ package com.enigmacamp.reservationcampus.controller;
 
 
 import com.enigmacamp.reservationcampus.model.entity.Profile;
+import com.enigmacamp.reservationcampus.model.entity.User;
 import com.enigmacamp.reservationcampus.model.request.AuthRequestStudent;
+import com.enigmacamp.reservationcampus.model.response.CommonResponse;
 import com.enigmacamp.reservationcampus.services.ProfileService;
 import com.enigmacamp.reservationcampus.services.UserService;
 import com.enigmacamp.reservationcampus.utils.constant.APIPath;
+import com.enigmacamp.reservationcampus.utils.constant.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +27,23 @@ public class UserController {
     public ResponseEntity<Profile> editUserProfileById(@PathVariable String id, @RequestBody AuthRequestStudent authRequestStudent) {
         Profile updatedProfile = profileService.updateProfileById(id, authRequestStudent);
         return ResponseEntity.ok(updatedProfile);
+    }
+
+    // UPDATE EMAIL & PASSWORD
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") String id, @RequestBody User user) {
+        String message = String.format(Message.MESSAGE_UPDATE);
+        User userUpdate = userService.editUser(id, user);
+
+        CommonResponse<User> response = CommonResponse.<User>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(message)
+                .data(userUpdate)
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
     }
 
 }
